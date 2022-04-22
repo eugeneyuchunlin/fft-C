10acbdd60b95b6d1e3abb4563bd2a4c44db5e4da
Eugene Lin <lin.eugene.l.e@gmail.com>
2022-04-22 11:16:59 +0800

    Add several test cases

--- verify.c
+++ verify.c
@@ -9,7 +9,7 @@ void test(int test_size);
 
 int main(){
     int test_cases[] = {1, 2, 3, 4, 5, 6, 7, 9, 10, 11, 12, 13, 14, 15, 16, 17, 
-    30, 7*11*10*13*31, 685464 };
+    30, 900, 1024, 1600, 2160, 3840, 4320, 7680, 7*11*10*13*31};
     int size = sizeof(test_cases)/sizeof(int);
     for(int i = 0; i < size; ++i){
         fprintf(stdout, "Running test case % 3d for size %- 10d...", i+1, test_cases[i]);
